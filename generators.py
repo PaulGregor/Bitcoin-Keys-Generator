6b6ad7fea354f347fa05f77f5933dfae297d6543
PaulGregor <comixan@gmail.com>
2020-02-19 00:08:40 +0200

    Removing requirements

--- generators.py
+++ generators.py
@@ -1,13 +1,13 @@
-import base58
-from binascii import hexlify, unhexlify
-import ecdsa
 import hashlib
+
+from binascii import hexlify, unhexlify
 from struct import Struct
+from utils import g, b58encode, b58decode
 
 PACKER = Struct('>QQQQ')
 
 
-def countLeadingZeroes(s):
+def count_leading_zeroes(s):
     count = 0
     for c in s:
         if c == '\0':
@@ -17,8 +17,7 @@ def countLeadingZeroes(s):
     return count
 
 
-def base58CheckEncode(prefix, payload, compressed=False):
-
+def base58_check_encode(prefix, payload, compressed=False):
     # Add version byte in front of RIPEMD-160 hash (0x00 for Main Network)
     s = prefix + payload
     if compressed:
@@ -29,14 +28,13 @@ def base58CheckEncode(prefix, payload, compressed=False):
 
     result = s + checksum
 
-    return '1' * countLeadingZeroes(result) + base58.b58encode(result)
+    return '1' * count_leading_zeroes(result) + b58encode(result).decode()
 
 
-def pubKeyToAddr(s):
+def pub_key_to_addr(s):
     ripemd160 = hashlib.new('ripemd160')
 
     hash_sha256 = hashlib.new('SHA256')
-    # bytearray.fromhex(s)
 
     # Perform SHA-256 hashing on the public key
     hash_sha256.update(bytes.fromhex(s))
@@ -44,13 +42,10 @@ def pubKeyToAddr(s):
     # Perform RIPEMD-160 hashing on the result of SHA-256
     ripemd160.update(hash_sha256.digest())
 
-    # return base58.b58encode(bytes.fromhex(ripemd160.hexdigest()))
-
-    return base58CheckEncode(b'\0', ripemd160.digest())
+    return base58_check_encode(b'\0', ripemd160.digest())
 
 
 def int_to_address(number):
-
     number0 = number >> 192
     number1 = (number >> 128) & 0xffffffffffffffff
     number2 = (number >> 64) & 0xffffffffffffffff
@@ -58,32 +53,26 @@ def int_to_address(number):
 
     private_key = hexlify(PACKER.pack(number0, number1, number2, number3)).decode("utf-8")
 
-    print(int(str(number), 16))
+    print(int(private_key, 16))
 
-    uncompressed_key = base58CheckEncode(b'\x80', unhexlify(private_key))
-    print(uncompressed_key + ' - uncompressed key')
+    uncompressed_key = base58_check_encode(b'\x80', unhexlify(private_key))
+    print(uncompressed_key + ' - private key')
 
-    # wif compressed key
-    compressed_key = base58CheckEncode(b'\x80', unhexlify(private_key), True)
-    print(compressed_key + ' - compressed key')
-
-    # public keys
-    sk = ecdsa.SigningKey.from_string(bytes.fromhex(private_key), curve=ecdsa.SECP256k1)
-
-    print(hexlify(b'\04' + sk.verifying_key.to_string()).decode('utf-8') + ' - uncompressed public key')
-
-    pk_prefix = b'\02'
-    if not int(hexlify(sk.verifying_key.to_string()[32:]), 16) % 2 == 0:
-        pk_prefix = b'\03'
-
-    print(hexlify(pk_prefix + sk.verifying_key.to_string()[:32]).decode('utf-8') + ' - compressed public key')
-
-    uncompressed_public_key = hexlify(b'\04' + sk.verifying_key.to_string()).decode('utf-8')
-    compressed_public_key = hexlify(pk_prefix + sk.verifying_key.to_string()[:32]).decode('utf-8')
     # address
+    x, y = str(g * int(private_key, 16)).split()
+    len1 = len(x)
+    len2 = len(y)
+    if len1 != 64:
+        z = 64 - len1
+        x = '0'*z + x
 
-    print(pubKeyToAddr(uncompressed_public_key) + ' - un compressed address')
-    print(pubKeyToAddr(compressed_public_key) + ' - compressed address')
+    if len2 != 64:
+        z = 64 - len2
+        y = '0'*z + y
+    uncompressed_public_key = '04' + x + y
+
+    print(uncompressed_public_key + ' - public key')
+    print(pub_key_to_addr(uncompressed_public_key) + ' - address')
 
 
 def wif_to_key(wif):
@@ -91,7 +80,9 @@ def wif_to_key(wif):
     if wif[0] in ['K', 'L']:
         slicer = 5
 
-    return hexlify(base58.b58decode(wif)[1:-slicer]).decode('utf-8')
+    return hexlify(b58decode(wif)[1:-slicer]).decode('utf-8')
 
-int_to_address(1)
 
+int_to_address(69302678922345667999943582182660846658111253030955767686930372293495671112904)
+
+#     print('1z4QU2cwJMorUq9ouUSbZNhHhQhCT4wRc')
